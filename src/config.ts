bef22927a59c2d81684d235bb437b4b7fec8c5ee
Stephan Auerhahn <palp@palp.org>
2023-07-04 15:38:43 -0700

    Return default config to avoid exceptions

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,18 @@ export function setConfig(config: ResolvedTraceConfig, ctx = context.active()) {
 	return ctx.setValue(configSymbol, config)
 }
 
+const defaultConfig: ResolvedTraceConfig = {
+	exporter: { export: () => Promise.resolve(), shutdown: () => Promise.resolve() },
+	fetch: { includeTraceContext: true },
+	postProcessor: spans => spans,
+	sampling: { headSampler: { shouldSample(context, traceId, spanName, spanKind, attributes, links) {
+		return { decision: 0, traceState: undefined, attributes: {} }
+	}, },
+	tailSampler: () => false,},
+	service: { name: 'unknown' },
+ }
+
 export function getActiveConfig(): ResolvedTraceConfig {
-	const config = context.active().getValue(configSymbol) as ResolvedTraceConfig
-	return config
+	const config = context.active().getValue(configSymbol) as ResolvedTraceConfig	
+	return config ?? defaultConfig
 }
